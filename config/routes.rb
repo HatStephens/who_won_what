5ad21c5e00df628e5e7878fa9fd08a17de619ffd
Stephen Giles <steve@steve-mac.local>
2015-06-04 21:23:37 +0100

    High Voltage and Devise added and set up. Users model created

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  # root 'pages#home'
+
+  get 'pages/home' => 'high_voltage/pages#show', id: 'home'
+  get 'pages/dashboard' => 'high_voltage/pages#show', id: 'dashboard'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
